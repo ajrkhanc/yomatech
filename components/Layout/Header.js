589eac53277b4446b86e71495ddeaa1bc1a9f9c4
Ajr khan <git config --global user.email you@example.com>
2022-09-12 17:59:52 +0530

    first commit

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -77,9 +77,9 @@ export default function Header() {
                                                 <li className='d-none'><a href="/services/web-development">Web Development</a></li>
                                             </ul>
                                         </li>
-                                        <li className='d-none'><a href="/why-yoma-tech">WHY YOMA TECH</a></li>                                  
-                                        <li  className='d-none'><a href="/why-india">WHY INDIA</a></li>
-                                        <li><a href="/blogs">BLOG</a></li>
+                                        <li><a href="/why-yoma-tech">WHY YOMA TECH</a></li>                                  
+                                        <li className='d-none'><a href="/why-india">WHY INDIA</a></li>
+                                        <li className='d-none'><a href="/blogs">BLOG</a></li>
                                         <li><a href="/contact-us">CONTACT US</a></li>
                                     </ul>
                                 </div>
